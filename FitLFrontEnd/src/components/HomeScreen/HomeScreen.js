bcd6ae7492de84d675da80b6ee01b8a3c738b9f3
Connor <connornusser@gmail.com>
2022-12-21 23:38:45 -0600

    add this change too

--- FitLFrontEnd/src/components/HomeScreen/HomeScreen.js
+++ FitLFrontEnd/src/components/HomeScreen/HomeScreen.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-HomeScreen.propTypes = {};
-HomeScreen.defaultProps = {};
-
-export default function HomeScreen() {
+export default function HomeScreen({props}) {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Settings!</Text>

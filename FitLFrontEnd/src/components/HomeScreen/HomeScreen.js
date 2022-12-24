970afca4e1c857d4554234a04ddbc9ad141f3dd3
Connor <connornusser@gmail.com>
2022-12-24 00:00:11 -0600

    yoyo

--- FitLFrontEnd/src/components/HomeScreen/HomeScreen.js
+++ FitLFrontEnd/src/components/HomeScreen/HomeScreen.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 export default function HomeScreen({props}) {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
+      <Text>HomeScreen!</Text>
     </View>
   );
 }
